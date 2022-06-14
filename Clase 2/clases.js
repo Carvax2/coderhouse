3ac20a1917ab839ee2919665816c3f1700795359
Carvax2 <62867091+Carvax2@users.noreply.github.com>
2022-06-13 20:56:00 -0500

    Arreglado formato de los comentarios

--- Clase 2/clases.js
+++ Clase 2/clases.js
@@ -7,34 +7,34 @@ class Usuario {
       this.mascotas = [];
     }
 
-    /*Devuelve el nombre completo de un usuario*/
+/*Devuelve el nombre completo de un usuario*/
     getFullName() {
         return `El nombre completo es ${this.nombre} ${this.apellido}`;
       }
     
-      /*Añade el nombre de una mascota a la lista de mascotas recibiendo el nombre como parametro*/
+/*Añade el nombre de una mascota a la lista de mascotas recibiendo el nombre como parametro*/
     addMascota(mascota) {
       this.mascotas.push(mascota);
     }
   
-    /*Cuenta la cantidad de mascotas en la lista de mascotas*/
+/*Cuenta la cantidad de mascotas en la lista de mascotas*/
     countMascotas() {
       return this.mascotas.length;
     }
   
-    /*Añade un libro a la lista de libros recibiendo como parametros el título y su autor */
+/*Añade un libro a la lista de libros recibiendo como parametros el título y su autor */
     addBook(titulo, autor) {
       this.titulo = titulo;
       this.autor = autor;
       this.libros.push({ titulo, autor });
     }
 
-    /*Devuelve el nombre de las mascotas de la lista de mascotas*/
+/*Devuelve el nombre de las mascotas de la lista de mascotas*/
     getMascotas() {
         return this.mascotas;
     }
 
-  /*Devuelve el titulo de cada uno de los libros de la lista de libros*/
+/*Devuelve el titulo de cada uno de los libros de la lista de libros*/
     getBooksNames() {
       const soloTitulos = this.libros.map(titulo => titulo.titulo);
       return soloTitulos;
